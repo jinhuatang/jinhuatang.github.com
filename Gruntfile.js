0b463f50e3bb22da016b5e4d56817019c2ce6e9c
Tang <JeviTang@gmail.com>
2013-04-02 10:49:46 +0800

    修改links

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,5 +30,25 @@
 
     // Default task(s).
     grunt.registerTask('default', ['markdown']);
-
+    grunt.registerTask('post', '', function () {
+        var posts = [];
+        grunt.file.recurse("_post", function (abspath, rootdir, subdir, filename) {
+            if (grunt.file.isFile(abspath)) {
+                var content = grunt.file.read(abspath);
+                var firstLineEnd = content.indexOf('\n');
+                var firstLine = content.substring(0, firstLineEnd).trim();
+                if (firstLine.length == 0) {
+                    console.error("没有内容：", filename);
+                }
+                var firstLineSegs = firstLine.split(' ');
+                posts.push({
+                    title: firstLineSegs[0],
+                    date: firstLineSegs[1]
+                });
+            }
+        });
+        posts.forEach(function (post) {
+            console.log(post.title, post.date);
+        });
+    });
 };
