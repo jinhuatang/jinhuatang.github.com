f576e96b5e243be9f0ed25592b38e6287386de98
Tang <JeviTang@gmail.com>
2013-04-10 18:35:30 +0800

    删除try catch

--- demo/uploadFile/tangram.uploadFile.js
+++ demo/uploadFile/tangram.uploadFile.js
@@ -21,7 +21,7 @@
         idoc.close();
         var me = this;
         iframe.onload = function () {
-            try {
+            //try {
                 var data = (iframe.contentDocument || iframe.contentWindow.document).body.innerHTML;
                 T.dom.remove(iframe);
                 if (type === 'json') {
@@ -33,7 +33,7 @@
                     }
                 }
                 callback && callback(data);
-            } catch (e) { window.console && console.dir(e); }
+            //} catch (e) { window.console && console.dir(e); }
             me.uploadFile(element, action, callback, type);
         };
     }
