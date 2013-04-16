0cc3429a1a4e7f4429d64dd5cbb7a40c109d6c0b
Tang <JeviTang@gmail.com>
2013-04-16 09:52:04 +0800

    修改demo

--- demo/uploadFile/jquery.uploadFile.js
+++ demo/uploadFile/jquery.uploadFile.js
@@ -1,4 +1,4 @@
-/*!
+/*
 * jQuery upload file Plugin 0.1.0
 *
 * Copyright 2013 Jevi Tang
@@ -6,7 +6,7 @@
 
 (function ($) {
     var iframeHTML = '<iframe frameborder="no" src="javascript:void(0)" scrolling="no" style="width:0px;height:0px;position:absolute;top:0;left:0;opacity:0;filter:alpha(opacity=0);"></iframe>'
-    var formHTML = '<body style="padding:0;margin:0;"><form style="padding:0;margin:0;" action="%ACTION%"><input type="file" style="cursor:pointer;padding:0;margin:0;font-size:1000px;width:1000px;" onchange="if(this.value){this.parentNode.submit();}"/></form></body>';
+    var formHTML = '<body style="padding:0;margin:0;"><form style="padding:0;margin:0;" enctype="multipart/form-data" method="POST" action="%ACTION%"><input type="file" name="file" style="cursor:pointer;padding:0;margin:0;font-size:1000px;width:1000px;" onchange="if(this.value){window.startUpload(window.abort);this.parentNode.submit();}"/></form></body>';
     var uploadFile = function (action, callback, type) {
         this.css('position', 'relative');
         var $iframe = $(iframeHTML);
@@ -14,10 +14,17 @@
         $iframe.height(this.height());
         this.append($iframe);
         var iframe = $iframe[0];
+        // for hidden element
+        var cloneElement = element.cloneNode(true);
+        cloneElement.style.position = 'absolute';
+        cloneElement.style.display = '';
+        cloneElement.style.top = '-1000px';
+        document.body.appendChild(cloneElement);
+        iframe.style.width = cloneElement.offsetWidth + 'px';
+        iframe.style.height = cloneElement.offsetHeight + 'px';
+        document.body.removeChild(cloneElement);
         var idoc = iframe.contentDocument || iframe.contentWindow.document;
-        idoc.open();
         idoc.write(formHTML.replace('%ACTION%', action));
-        idoc.close();
         var me = this;
         $iframe.bind('load', function () {
             try {
@@ -31,10 +38,25 @@
                         eval('data=' + data);
                     }
                 }
-                callback && callback(data);
-            } catch (e) { window.console && console.dir(e); }
-            me.uploadFile(action, callback, type);
+            } catch (e) {
+                window.console && console.dir(e);
+                if (type === 'json') {
+                    data = { status: 1, msg: "返回值错误" };
+                } else {
+                    data = "返回值错误";
+                }
+            }
+            endCallback && endCallback(data);
+            me.uploadFile(action, startCallback, endCallback, type);
         });
+        if (startCallback) {
+            iframe.contentWindow.startUpload = startCallback;
+        }
+
+        iframe.contentWindow.abort = function () {
+            T.dom.remove(iframe);
+            me.uploadFile(action, startCallback, endCallback, type);
+        }
     }
     $.fn.uploadFile = uploadFile;
 } (jQuery));
--- demo/uploadFile/tangram.uploadFile.js
+++ demo/uploadFile/tangram.uploadFile.js
@@ -1,24 +1,30 @@
-/*!
-* tangram upload file Plugin 0.1.0
-*
-* Copyright 2013 Jevi Tang
-*/
+/*
+ * tangram upload file Plugin 0.1.0
+ *
+ * Copyright 2013 Jevi Tang
+ */
 
 (function (T) {
     var iframeHTML = '<iframe frameborder="no" src="javascript:void(0)" scrolling="no" style="width:0px;height:0px;position:absolute;top:0;left:0;opacity:0;filter:alpha(opacity=0);"></iframe>'
-    var formHTML = '<body style="padding:0;margin:0;"><form style="padding:0;margin:0;" action="%ACTION%"><input type="file" style="cursor:pointer;padding:0;margin:0;font-size:1000px;width:1000px;" onchange="if(this.value){this.parentNode.submit();}"/></form></body>';
-    var uploadFile = function (element, action, callback, type) {
+    var formHTML = '<body style="padding:0;margin:0;"><form style="padding:0;margin:0;" enctype="multipart/form-data" method="POST" action="%ACTION%"><input type="file" name="file" style="cursor:pointer;padding:0;margin:0;font-size:1000px;width:1000px;" onchange="if(this.value){window.startUpload(window.abort);this.parentNode.submit();}"/></form></body>';
+    var uploadFile = function (element, action, startCallback, endCallback, type) {
         element = T.g(element);
         element.style.position = 'relative';
         T.dom.insertHTML(element, 'beforeEnd', iframeHTML);
         var $iframe = T.dom.query('iframe', element);
         var iframe = $iframe[0];
-        iframe.style.width = element.offsetWidth + 'px';
-        iframe.style.height = element.offsetHeight + 'px';
+        // for hidden element
+        var cloneElement = element.cloneNode(true);
+        cloneElement.style.position = 'absolute';
+        cloneElement.style.display = '';
+        cloneElement.style.top = '-1000px';
+        document.body.appendChild(cloneElement);
+        iframe.style.width = cloneElement.offsetWidth + 'px';
+        iframe.style.height = cloneElement.offsetHeight + 'px';
+        document.body.removeChild(cloneElement);
+
         var idoc = iframe.contentDocument || iframe.contentWindow.document;
-        idoc.open();
         idoc.write(formHTML.replace('%ACTION%', action));
-        idoc.close();
         T.on(iframe, 'load', function () {
             try {
                 var data = (iframe.contentDocument || iframe.contentWindow.document).body.innerHTML;
@@ -31,10 +37,27 @@
                         eval('data=' + data);
                     }
                 }
-                callback && callback(data);
-            } catch (e) { window.console && console.dir(e); }
-            T.uploadFile(element, action, callback, type);
+            } catch (e) {
+                window.console && console.dir(e);
+                if (type === 'json') {
+                    data = { status: 1, msg: "返回值错误" };
+                } else {
+                    data = "返回值错误";
+                }
+            }
+            endCallback && endCallback(data);
+            T.uploadFile(element, action, startCallback, endCallback, type);
         });
+
+        if (startCallback) {
+            iframe.contentWindow.startUpload = startCallback;
+        }
+
+        iframe.contentWindow.abort = function () {
+            T.dom.remove(iframe);
+            T.uploadFile(element, action, startCallback, endCallback, type);
+        }
     }
+
     T.uploadFile = uploadFile;
 } (baidu));
